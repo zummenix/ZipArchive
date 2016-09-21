33bb2b4d97b9aa12d36d91752ec6bfc61b030d94
Joshua Hudson <hudsonj@wsdot.wa.gov>
2016-09-21 08:46:10 -0700

    Add Swift 2.3 support for main project code. Test app is still in Swift 3

--- SSZipArchive/SSZipArchive+Swift.swift
+++ SSZipArchive/SSZipArchive+Swift.swift
@@ -10,12 +10,13 @@ import Foundation
 
 extension SSZipArchive {
     
-    static func unzipFileAtPath(_ path: String, toDestination destination: String, overwrite: Bool, password: String?, delegate: SSZipArchiveDelegate?) throws -> Bool {
+    static func unzipFileAtPath(path: String, toDestination destination: String, overwrite: Bool, password: String?, delegate: SSZipArchiveDelegate?) throws -> Bool {
         
         var success = false
         var error: NSError?
         
-        success = __unzipFile(atPath: path, toDestination: destination, overwrite: overwrite, password: password, error: &error, delegate: delegate)
+        success = __unzipFileAtPath(path, toDestination: destination, overwrite: overwrite, password: password, error: &error, delegate: delegate)
+        
         if let throwableError = error {
             throw throwableError
         }
